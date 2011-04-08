3e5dbdf229211d8a75f2763f9fcb73f7dbef79fe
Stefan <stefan@boxbox.org>
2011-04-08 13:47:09 +0200

    Switched license to zlib. Removed debris in sfg::Button.
    Signed-off-by: Stefan <stefan@boxbox.org>

--- include/SFGUI/Button.hpp
+++ include/SFGUI/Button.hpp
@@ -53,7 +53,6 @@ class Button : public Bin {
 		bool HandleMouseButtonPress( Widget::Ptr widget, int x, int y, sf::Mouse::Button button );
 		bool HandleMouseButtonRelease( Widget::Ptr widget, int x, int y, sf::Mouse::Button button );
 		void HandleSizeAllocate( Widget::Ptr widget, const sf::FloatRect& old_allocation );
-		void HandlePositionChange( Widget::Ptr widget, const sf::FloatRect& old_allocation );
 
 		Label::Ptr  m_label;
 		float m_padding;
