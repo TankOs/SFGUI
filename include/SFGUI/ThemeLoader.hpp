e5c9be4d9b5e6bc3ae36dd4db0490bbb52e5d0dd
Antti Tuppurainen <atbaaba@gmail.com>
2011-05-02 19:17:15 +0300

    Mark Observer::Observer() and operator() inline to avoid multiple definition errors.

--- include/SFGUI/ThemeLoader.hpp
+++ include/SFGUI/ThemeLoader.hpp
@@ -23,8 +23,8 @@ class SFGUI_API ThemeLoader {
 
 	private:
 		struct Observer : public KeyValueLoader::Observer {
-			Observer( Engine& engine_ );
-			void operator()( const std::string& key, const std::string& value );
+			inline Observer( Engine& engine_ );
+			inline void operator()( const std::string& key, const std::string& value );
 
 			Engine& engine;
 		};
