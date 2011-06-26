0e3a9d5b03c28b25ed3e8bc59aada4a081d4780a
Stefan Schindler <stefan@boxbox.org>
2011-06-26 15:47:13 +0200

    Removed SCons files. Adjusted source to build successfully on Windows. Added Boost dependency in CMake file.
    Signed-off-by: Stefan Schindler <stefan@boxbox.org>

--- include/SFGUI/Config.hpp
+++ include/SFGUI/Config.hpp
@@ -1,7 +1,18 @@
 #ifndef _SFGUI_CONFIG_HPP__
 #define _SFGUI_CONFIG_HPP__
 
-#define SFGUI_API
+#if defined( _WIN32 ) || defined( __WIN32__ )
+	#define WIN32_LEAN_AND_MEAN
+	#define NOMINMAX
+
+	#ifdef SFGUI_EXPORTS
+		#define SFGUI_API __declspec( dllexport )
+	#else
+		#define SFGUI_API __declspec( dllimport )
+	#endif
+#else
+	#define SFGUI_API
+#endif
 
 #include <iostream> // XXX Only for debugging purposes.
 
