2b8173b17e152fa994e59243bd65c5a3ccbe82fc
binary1248 <binary1248@hotmail.com>
2011-08-22 18:46:29 +0200

    Value parameter first when calling Adjustment constructor. Added ValueChange signal to Adjustment class.

--- include/SFGUI/Adjustment.hpp
+++ include/SFGUI/Adjustment.hpp
@@ -11,18 +11,21 @@ class SFGUI_API Adjustment : public Object {
 		typedef boost::shared_ptr<Adjustment>  Ptr; //!< Shared pointer.
 
 		/** Ctor.
+		 * @param value Current value.
 		 * @param lower Minimum value.
 		 * @param upper Maximum value.
-		 * @param value Current value.
 		 * @param step Minor change value (such as clicking on arrow button).
 		 * @param page Major change value (such as clicking on the scroll area).
 		 * @param size Page size (how many entries are visible / slider size).
 		 */
-		Adjustment( float lower, float upper, float value, float step, float page, float size );
+		Adjustment( float value, float lower, float upper, float step, float page, float size );
 
 		/** Dtor.
 		 */
 		~Adjustment();
+
+		// Signals.
+		Signal<void( Ptr )>  OnValueChange; //!< Fired when value changed.
 
 	private:
 		float m_lower;
--- src/Adjustment.cpp
+++ src/Adjustment.cpp
@@ -2,10 +2,10 @@
 
 namespace sfg {
 
-Adjustment::Adjustment( float lower, float upper, float value, float step, float page, float size ) :
+Adjustment::Adjustment( float value, float lower, float upper, float step, float page, float size ) :
+	m_value( value ),
 	m_lower( lower ),
 	m_upper( upper ),
-	m_value( value ),
 	m_step( step ),
 	m_page( page ),
 	m_size( size )
