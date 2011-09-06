c9aefc2c084bfe1385c771d9a4f8e4a0cbb93543
root <root@DIMS-MACHINE.localdomain>
2011-09-06 20:50:50 +0200

    Changed RenderQueue container type to boost::ptr_vector. The features of boost::ptr_list were not needed and thus boost::ptr_vector is more efficient (space and time).

--- src/SFGUI/RenderQueue.cpp
+++ src/SFGUI/RenderQueue.cpp
@@ -8,11 +8,8 @@ void RenderQueue::Add( sf::Drawable* drawable ) {
 }
 
 void RenderQueue::Render( sf::RenderTarget& target, sf::Renderer& /*renderer*/ ) const {
-	DrawablesList::const_iterator  citer( m_children.begin() );
-	DrawablesList::const_iterator  citerend( m_children.end() );
-
-	for( ; citer != citerend; ++citer ) {
-		target.Draw( *citer );
+	for( DrawablesVector::size_type i = 0; i < m_children.size(); i++ ) {
+		target.Draw( m_children[i] );
 	}
 }
 
