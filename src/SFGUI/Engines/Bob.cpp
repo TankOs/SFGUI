4b40dd6142924b15f13e5dbfa9818a0ea5f9c0ad
Patrick Winkler <patrick.winkler@intergga.ch>
2013-02-02 20:57:00 +0100

    Add the Imaged widget to Bob

--- src/SFGUI/Engines/Bob.cpp
+++ src/SFGUI/Engines/Bob.cpp
@@ -1,26 +1,6 @@
 #include <SFGUI/Engines/Bob.hpp>
-#include <SFGUI/RenderQueue.hpp>
-#include <SFGUI/Context.hpp>
 #include <SFGUI/Renderer.hpp>
-#include <SFGUI/Window.hpp>
-#include <SFGUI/Button.hpp>
-#include <SFGUI/Entry.hpp>
-#include <SFGUI/Scale.hpp>
-#include <SFGUI/Scrollbar.hpp>
-#include <SFGUI/ScrolledWindow.hpp>
-#include <SFGUI/ToggleButton.hpp>
-#include <SFGUI/CheckButton.hpp>
-#include <SFGUI/ProgressBar.hpp>
-#include <SFGUI/Separator.hpp>
-#include <SFGUI/Frame.hpp>
-#include <SFGUI/Image.hpp>
-#include <SFGUI/Notebook.hpp>
-#include <SFGUI/Spinner.hpp>
-#include <SFGUI/ComboBox.hpp>
 
-#include <SFML/Graphics/ConvexShape.hpp>
-#include <SFML/Graphics/Text.hpp>
-#include <SFML/Graphics/Sprite.hpp>
 #include <cmath>
 
 namespace sfg {
--- src/SFGUI/Engines/Bob/Image.cpp
+++ src/SFGUI/Engines/Bob/Image.cpp
@@ -6,8 +6,24 @@
 namespace sfg {
 namespace eng {
 
-RenderQueue* Bob::CreateImageDrawable( SharedPtr<const Image> /*image*/ ) const {
-	return NULL;
+RenderQueue* Bob::CreateImageDrawable( SharedPtr<const Image> image ) const {
+	sf::Color background_color( GetProperty<sf::Color>( "BackgroundColor", image ) );
+
+	RenderQueue* queue( new RenderQueue );
+
+	queue->Add(
+		Renderer::Get().CreateImage(
+			sf::FloatRect(
+				( image->GetAllocation().width - image->GetRequisition().x ) * image->GetAlignment().x,
+				( image->GetAllocation().height - image->GetRequisition().y ) * image->GetAlignment().y,
+				static_cast<float>( image->GetImage().getSize().x ),
+				static_cast<float>( image->GetImage().getSize().y )
+			),
+			image->GetImage()
+		)
+	);
+
+	return queue;
 }
 
 }
