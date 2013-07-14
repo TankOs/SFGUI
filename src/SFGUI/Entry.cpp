5082a2acbc31247a32424f5e1ba811d53c64ba03
binary1248 <binary1248@hotmail.com>
2013-07-14 09:39:37 +0200

    Fixed appending and prepending text to an Entry not properly invalidating it.

--- src/SFGUI/Entry.cpp
+++ src/SFGUI/Entry.cpp
@@ -49,11 +49,11 @@ const sf::String& Entry::GetText() const {
 }
 
 void Entry::AppendText( const sf::String& text ) {
-	m_string += text;
+	SetText( m_string + text );
 }
 
 void Entry::PrependText( const sf::String& text ) {
-	m_string = text + m_string;
+	SetText( text + m_string );
 }
 
 void Entry::SetCursorPosition( std::size_t new_position ) {
